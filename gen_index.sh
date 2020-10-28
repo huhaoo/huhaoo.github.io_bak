7c3a640fa2e5101b130fde030604d9ca0a050bc3
huhaoo <qq826538400@gmail.com>
2020-10-28 15:32:12 +0800

    update

---
+++ gen_index.sh
@@ -0,0 +1,22 @@
+if [ -f index.md ]
+then
+	rm index.md
+fi
+touch index.md
+
+find md_pub/ -name "[!.]*" | while read i
+do
+	if [ -f $i ]
+	then
+		echo>>index.md "[$i]($i)"
+	fi
+done
+find files/ -name "[!.]*" | while read i
+do
+
+	if [ -f $i ]
+	then
+		echo>>index.md "[$i]($i)"
+	fi
+
+done
--- up.sh
+++ up.sh
@@ -1,4 +1,5 @@
 bash latex2pic.sh
+bash gen_index.sh
 git add *
 git commit -m "update"
 git push blog master # Init: git remote add blog https://github.com/huhaoo/huhaoo.github.io.git
